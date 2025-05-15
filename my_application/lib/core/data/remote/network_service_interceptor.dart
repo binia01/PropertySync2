23d902d7a6b8d146fbbd4846cb590ba79c618dfb
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-05-15 23:44:03 +0300

    Refactor network service and interceptor to use token-based authentication and improve error handling

--- my_application/lib/core/data/remote/network_service_interceptor.dart
+++ my_application/lib/core/data/remote/network_service_interceptor.dart
@@ -1,30 +1,46 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:my_application/core/data/local/secure_storage/isecure_storage.dart';
-import 'package:my_application/core/data/local/secure_storage/secure_storage.dart';
-import 'package:my_application/core/data/local/secure_storage/secure_storage_const.dart';
+import 'package:my_application/core/data/remote/token/itoken_service.dart';
+import 'package:my_application/core/data/remote/token/token_service.dart';
 
-final networkServiceInterceptorProvider = Provider<NetworkServiceInterceptor>((
-  ref,
-) {
-  final secureStorage = ref.watch(secureStorageProvider);
-  return NetworkServiceInterceptor(secureStorage);
-});
+final networkServiceInterceptorProvider =
+    Provider.family<NetworkServiceInterceptor, Dio>((ref, dio) {
+      final tokenService = ref.watch(tokenServiceProvider(dio));
+      return NetworkServiceInterceptor(tokenService, dio);
+    });
 
 final class NetworkServiceInterceptor extends Interceptor {
-  final IsecureStorage _secureStorage;
+  final ITokenService _tokenService;
+  final Dio _dio;
 
-  NetworkServiceInterceptor(this._secureStorage);
+  NetworkServiceInterceptor(this._tokenService, this._dio);
 
   @override
   Future<void> onRequest(
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) async {
-    final accessToken = await _secureStorage.read(accessTokenKey);
+    final accessToken = await _tokenService.getAccessToken();
     options.headers['Content-Type'] = 'application/json';
     options.headers['Accept'] = 'application/json';
     options.headers['Authorization'] = 'Bearer $accessToken';
     super.onRequest(options, handler);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    final token = await _tokenService.getRefreshToken();
+    try {
+      final accessToken = await _tokenService.refeshToken(token);
+      final options = err.requestOptions;
+      options.headers['Authorization'] = 'Bearer $accessToken';
+      return handler.resolve(await _dio.fetch(options));
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) {
+        await _tokenService.clearToken();
+        return handler.next(e);
+      }
+      return handler.next(e);
+    }
   }
 }
