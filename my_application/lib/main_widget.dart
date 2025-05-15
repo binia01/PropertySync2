9097d7cb9ac57a4fcf2399aebacd20daa10da784
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-05-15 17:27:44 +0300

    Refactor main application structure to use MainWidget and update widget tests

---
+++ my_application/lib/main_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_application/core/route/go_router_provider.dart';
+
+class MainWidget extends ConsumerWidget {
+  const MainWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(goRouterProvider);
+    return MaterialApp.router(routerConfig: goRouter);
+  }
+}
