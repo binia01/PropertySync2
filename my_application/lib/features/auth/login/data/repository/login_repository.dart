a0ee8e655e0072f7f095c3ded385541fc217c24b
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-05-22 17:42:52 +0300

    feat: Integrate token service into LoginRepository for token management

--- my_application/lib/features/auth/login/data/repository/login_repository.dart
+++ my_application/lib/features/auth/login/data/repository/login_repository.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_application/core/data/remote/network_service.dart';
+import 'package:my_application/core/data/remote/token/itoken_service.dart';
+import 'package:my_application/core/data/remote/token/token_service.dart';
 import 'package:my_application/features/auth/login/data/dto/request/login_request.dart';
 import 'package:my_application/features/auth/login/data/dto/response/login_response.dart';
 import 'package:my_application/features/auth/login/data/repository/ilogin_repository.dart';
@@ -7,18 +10,23 @@ import 'package:my_application/features/auth/login/data/source/remote/login_api.
 
 final loginRepositoryProvider = Provider<IloginRepository>((ref) {
   final loginApi = ref.watch(loginApiProvider);
-  return LoginRepository(loginApi);
+  final tokenService = ref.watch(
+    tokenServiceProvider(ref.watch(networkServiceProvider)),
+  );
+  return LoginRepository(loginApi, tokenService);
 });
 
 final class LoginRepository implements IloginRepository {
   final LoginApi _loginApi;
+  final ITokenService _tokenService;
 
-  LoginRepository(this._loginApi);
+  LoginRepository(this._loginApi, this._tokenService);
 
   @override
   Future<LoginResponse> login(LoginRequest data) async {
     try {
       final response = await _loginApi.login(data);
+      await _tokenService.saveToken(response.accessToken);
       return response;
     } on DioException catch (_) {
       rethrow;
