ecf1149fd720870dd73a8b4199a695224417b2b3
Binyam Mezemir <binyam.mezemir-ug@aau.edu.et>
2025-06-08 22:12:15 +0300

    done with appointment model with the immutable data class

---
+++ my_application/lib/features/appointment/domain/model/appointment_model.freezed.dart
@@ -0,0 +1,385 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'appointment_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+  'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models',
+);
+
+/// @nodoc
+mixin _$AppointmentModel {
+  @JsonKey(name: "id")
+  int? get id => throw _privateConstructorUsedError;
+  @JsonKey(name: "createdAt")
+  DateTime? get createdAt => throw _privateConstructorUsedError;
+  @JsonKey(name: "updatedAt")
+  DateTime? get updatedAt => throw _privateConstructorUsedError;
+  @JsonKey(name: "startTime")
+  DateTime? get startTime => throw _privateConstructorUsedError;
+  @JsonKey(name: "Date")
+  DateTime? get date => throw _privateConstructorUsedError; // fixed key
+  @JsonKey(name: "propertyId")
+  int? get propertyId => throw _privateConstructorUsedError;
+  @JsonKey(name: "buyerId")
+  int? get buyerId => throw _privateConstructorUsedError;
+  @JsonKey(name: "sellerId")
+  int? get sellerId => throw _privateConstructorUsedError;
+  @JsonKey(name: "status")
+  String? get status => throw _privateConstructorUsedError;
+
+  /// Create a copy of AppointmentModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $AppointmentModelCopyWith<AppointmentModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AppointmentModelCopyWith<$Res> {
+  factory $AppointmentModelCopyWith(
+    AppointmentModel value,
+    $Res Function(AppointmentModel) then,
+  ) = _$AppointmentModelCopyWithImpl<$Res, AppointmentModel>;
+  @useResult
+  $Res call({
+    @JsonKey(name: "id") int? id,
+    @JsonKey(name: "createdAt") DateTime? createdAt,
+    @JsonKey(name: "updatedAt") DateTime? updatedAt,
+    @JsonKey(name: "startTime") DateTime? startTime,
+    @JsonKey(name: "Date") DateTime? date,
+    @JsonKey(name: "propertyId") int? propertyId,
+    @JsonKey(name: "buyerId") int? buyerId,
+    @JsonKey(name: "sellerId") int? sellerId,
+    @JsonKey(name: "status") String? status,
+  });
+}
+
+/// @nodoc
+class _$AppointmentModelCopyWithImpl<$Res, $Val extends AppointmentModel>
+    implements $AppointmentModelCopyWith<$Res> {
+  _$AppointmentModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of AppointmentModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? createdAt = freezed,
+    Object? updatedAt = freezed,
+    Object? startTime = freezed,
+    Object? date = freezed,
+    Object? propertyId = freezed,
+    Object? buyerId = freezed,
+    Object? sellerId = freezed,
+    Object? status = freezed,
+  }) {
+    return _then(
+      _value.copyWith(
+            id:
+                freezed == id
+                    ? _value.id
+                    : id // ignore: cast_nullable_to_non_nullable
+                        as int?,
+            createdAt:
+                freezed == createdAt
+                    ? _value.createdAt
+                    : createdAt // ignore: cast_nullable_to_non_nullable
+                        as DateTime?,
+            updatedAt:
+                freezed == updatedAt
+                    ? _value.updatedAt
+                    : updatedAt // ignore: cast_nullable_to_non_nullable
+                        as DateTime?,
+            startTime:
+                freezed == startTime
+                    ? _value.startTime
+                    : startTime // ignore: cast_nullable_to_non_nullable
+                        as DateTime?,
+            date:
+                freezed == date
+                    ? _value.date
+                    : date // ignore: cast_nullable_to_non_nullable
+                        as DateTime?,
+            propertyId:
+                freezed == propertyId
+                    ? _value.propertyId
+                    : propertyId // ignore: cast_nullable_to_non_nullable
+                        as int?,
+            buyerId:
+                freezed == buyerId
+                    ? _value.buyerId
+                    : buyerId // ignore: cast_nullable_to_non_nullable
+                        as int?,
+            sellerId:
+                freezed == sellerId
+                    ? _value.sellerId
+                    : sellerId // ignore: cast_nullable_to_non_nullable
+                        as int?,
+            status:
+                freezed == status
+                    ? _value.status
+                    : status // ignore: cast_nullable_to_non_nullable
+                        as String?,
+          )
+          as $Val,
+    );
+  }
+}
+
+/// @nodoc
+abstract class _$$AppointmentModelImplCopyWith<$Res>
+    implements $AppointmentModelCopyWith<$Res> {
+  factory _$$AppointmentModelImplCopyWith(
+    _$AppointmentModelImpl value,
+    $Res Function(_$AppointmentModelImpl) then,
+  ) = __$$AppointmentModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({
+    @JsonKey(name: "id") int? id,
+    @JsonKey(name: "createdAt") DateTime? createdAt,
+    @JsonKey(name: "updatedAt") DateTime? updatedAt,
+    @JsonKey(name: "startTime") DateTime? startTime,
+    @JsonKey(name: "Date") DateTime? date,
+    @JsonKey(name: "propertyId") int? propertyId,
+    @JsonKey(name: "buyerId") int? buyerId,
+    @JsonKey(name: "sellerId") int? sellerId,
+    @JsonKey(name: "status") String? status,
+  });
+}
+
+/// @nodoc
+class __$$AppointmentModelImplCopyWithImpl<$Res>
+    extends _$AppointmentModelCopyWithImpl<$Res, _$AppointmentModelImpl>
+    implements _$$AppointmentModelImplCopyWith<$Res> {
+  __$$AppointmentModelImplCopyWithImpl(
+    _$AppointmentModelImpl _value,
+    $Res Function(_$AppointmentModelImpl) _then,
+  ) : super(_value, _then);
+
+  /// Create a copy of AppointmentModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? createdAt = freezed,
+    Object? updatedAt = freezed,
+    Object? startTime = freezed,
+    Object? date = freezed,
+    Object? propertyId = freezed,
+    Object? buyerId = freezed,
+    Object? sellerId = freezed,
+    Object? status = freezed,
+  }) {
+    return _then(
+      _$AppointmentModelImpl(
+        id:
+            freezed == id
+                ? _value.id
+                : id // ignore: cast_nullable_to_non_nullable
+                    as int?,
+        createdAt:
+            freezed == createdAt
+                ? _value.createdAt
+                : createdAt // ignore: cast_nullable_to_non_nullable
+                    as DateTime?,
+        updatedAt:
+            freezed == updatedAt
+                ? _value.updatedAt
+                : updatedAt // ignore: cast_nullable_to_non_nullable
+                    as DateTime?,
+        startTime:
+            freezed == startTime
+                ? _value.startTime
+                : startTime // ignore: cast_nullable_to_non_nullable
+                    as DateTime?,
+        date:
+            freezed == date
+                ? _value.date
+                : date // ignore: cast_nullable_to_non_nullable
+                    as DateTime?,
+        propertyId:
+            freezed == propertyId
+                ? _value.propertyId
+                : propertyId // ignore: cast_nullable_to_non_nullable
+                    as int?,
+        buyerId:
+            freezed == buyerId
+                ? _value.buyerId
+                : buyerId // ignore: cast_nullable_to_non_nullable
+                    as int?,
+        sellerId:
+            freezed == sellerId
+                ? _value.sellerId
+                : sellerId // ignore: cast_nullable_to_non_nullable
+                    as int?,
+        status:
+            freezed == status
+                ? _value.status
+                : status // ignore: cast_nullable_to_non_nullable
+                    as String?,
+      ),
+    );
+  }
+}
+
+/// @nodoc
+
+class _$AppointmentModelImpl implements _AppointmentModel {
+  const _$AppointmentModelImpl({
+    @JsonKey(name: "id") this.id,
+    @JsonKey(name: "createdAt") this.createdAt,
+    @JsonKey(name: "updatedAt") this.updatedAt,
+    @JsonKey(name: "startTime") this.startTime,
+    @JsonKey(name: "Date") this.date,
+    @JsonKey(name: "propertyId") this.propertyId,
+    @JsonKey(name: "buyerId") this.buyerId,
+    @JsonKey(name: "sellerId") this.sellerId,
+    @JsonKey(name: "status") this.status,
+  });
+
+  @override
+  @JsonKey(name: "id")
+  final int? id;
+  @override
+  @JsonKey(name: "createdAt")
+  final DateTime? createdAt;
+  @override
+  @JsonKey(name: "updatedAt")
+  final DateTime? updatedAt;
+  @override
+  @JsonKey(name: "startTime")
+  final DateTime? startTime;
+  @override
+  @JsonKey(name: "Date")
+  final DateTime? date;
+  // fixed key
+  @override
+  @JsonKey(name: "propertyId")
+  final int? propertyId;
+  @override
+  @JsonKey(name: "buyerId")
+  final int? buyerId;
+  @override
+  @JsonKey(name: "sellerId")
+  final int? sellerId;
+  @override
+  @JsonKey(name: "status")
+  final String? status;
+
+  @override
+  String toString() {
+    return 'AppointmentModel(id: $id, createdAt: $createdAt, updatedAt: $updatedAt, startTime: $startTime, date: $date, propertyId: $propertyId, buyerId: $buyerId, sellerId: $sellerId, status: $status)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$AppointmentModelImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt) &&
+            (identical(other.updatedAt, updatedAt) ||
+                other.updatedAt == updatedAt) &&
+            (identical(other.startTime, startTime) ||
+                other.startTime == startTime) &&
+            (identical(other.date, date) || other.date == date) &&
+            (identical(other.propertyId, propertyId) ||
+                other.propertyId == propertyId) &&
+            (identical(other.buyerId, buyerId) || other.buyerId == buyerId) &&
+            (identical(other.sellerId, sellerId) ||
+                other.sellerId == sellerId) &&
+            (identical(other.status, status) || other.status == status));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+    runtimeType,
+    id,
+    createdAt,
+    updatedAt,
+    startTime,
+    date,
+    propertyId,
+    buyerId,
+    sellerId,
+    status,
+  );
+
+  /// Create a copy of AppointmentModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$AppointmentModelImplCopyWith<_$AppointmentModelImpl> get copyWith =>
+      __$$AppointmentModelImplCopyWithImpl<_$AppointmentModelImpl>(
+        this,
+        _$identity,
+      );
+}
+
+abstract class _AppointmentModel implements AppointmentModel {
+  const factory _AppointmentModel({
+    @JsonKey(name: "id") final int? id,
+    @JsonKey(name: "createdAt") final DateTime? createdAt,
+    @JsonKey(name: "updatedAt") final DateTime? updatedAt,
+    @JsonKey(name: "startTime") final DateTime? startTime,
+    @JsonKey(name: "Date") final DateTime? date,
+    @JsonKey(name: "propertyId") final int? propertyId,
+    @JsonKey(name: "buyerId") final int? buyerId,
+    @JsonKey(name: "sellerId") final int? sellerId,
+    @JsonKey(name: "status") final String? status,
+  }) = _$AppointmentModelImpl;
+
+  @override
+  @JsonKey(name: "id")
+  int? get id;
+  @override
+  @JsonKey(name: "createdAt")
+  DateTime? get createdAt;
+  @override
+  @JsonKey(name: "updatedAt")
+  DateTime? get updatedAt;
+  @override
+  @JsonKey(name: "startTime")
+  DateTime? get startTime;
+  @override
+  @JsonKey(name: "Date")
+  DateTime? get date; // fixed key
+  @override
+  @JsonKey(name: "propertyId")
+  int? get propertyId;
+  @override
+  @JsonKey(name: "buyerId")
+  int? get buyerId;
+  @override
+  @JsonKey(name: "sellerId")
+  int? get sellerId;
+  @override
+  @JsonKey(name: "status")
+  String? get status;
+
+  /// Create a copy of AppointmentModel
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$AppointmentModelImplCopyWith<_$AppointmentModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
