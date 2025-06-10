7b50a3039886ecd0684a3a834f2016aabb0aaccb
Kibreab Chanyalew <affablemelon@gmail.com>
2025-06-10 16:42:16 +0300

    main app entry, clear caches, pass in contexts needed.

--- my_application/lib/main_widget.dart
+++ my_application/lib/main_widget.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_application/core/providers/user.role.provider.dart';
 import 'package:my_application/core/route/go_router_provider.dart';
 
 class MainWidget extends ConsumerWidget {
   const MainWidget({super.key});
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final goRouter = ref.watch(goRouterProvider);
-    return MaterialApp.router(routerConfig: goRouter);
+
+    // ref.listen<AsyncValue<String?>>(userRoleProvider, (previous, next) {
+    //   debugPrint("MyApp: userRoleProvider state changed! Previous: ${previous?.value}, Next: ${next.value}, Is Loading: ${next.isLoading}");
+    // });
+
+
+    return MaterialApp.router(
+      routerConfig: goRouter,
+      title: 'RBAC App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.black,
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+    );
   }
 }
